66ee65c793162a16018fdbfaae604de5905edfd8
sridharmohanty070-dotcom <sridharmohanty070@gmail.com>
2026-08-14 22:14:25 +0530

    "Insert time"

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const contact = document.getElementById("contact");
 contact.addEventListener('click', ()=>{
     alert("Please contact:   sridharmohanty070@gmail.com");
 });
+
+const div = document.querySelector('#time');
+setInterval(()=>{
+    let time = new Date();
+    div.textContent = time.toLocaleTimeString();
+},1000);
